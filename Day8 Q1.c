d166414edf2841c39485b58e3c4fb922b0a05d16
Saksham-Jain-2007 <sakshamjain7017@gmail.com>
2026-02-19 09:21:34 +0530

    Create Day8 Q1.c

---
+++ Day8 Q1.c
@@ -0,0 +1,23 @@
+// Question: Given integers a and b, compute a^b using recursion without using pow() function.
+
+#include <stdio.h>
+
+// Recursive power function
+int power(int a, int b) {
+    if (b == 0)
+        return 1;          // Base case: a^0 = 1
+
+    return a * power(a, b - 1);
+}
+
+int main() {
+    int a, b;
+
+    // Read input
+    scanf("%d %d", &a, &b);
+
+    // Print result
+    printf("%d", power(a, b));
+
+    return 0;
+}
