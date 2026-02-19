5241fc722cabb54c576fdabdc751886f1b421f6c
Saksham-Jain-2007 <sakshamjain7017@gmail.com>
2026-02-19 09:26:24 +0530

    Implement matrix addition for two input matrices
    The program reads two matrices of the same dimensions and outputs their sum by adding corresponding elements.

---
+++ Day11 Q1.c
@@ -0,0 +1,36 @@
+// Question:  Write a program to perform addition of two matrices having the same dimensions. The sum of two matrices is obtained by adding corresponding elements of the matrices.
+
+#include <stdio.h>
+
+int main() {
+    int m, n;
+
+    // Read rows and columns
+    scanf("%d %d", &m, &n);
+
+    int A[m][n], B[m][n];
+
+    // Read first matrix
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    // Read second matrix
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    // Add and print result
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", A[i][j] + B[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
