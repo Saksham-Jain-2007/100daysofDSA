2e0c5a3fd75de56ba9f21f505919e14d1db0afdb
Saksham-Jain-2007 <sakshamjain7017@gmail.com>
2026-03-21 12:21:02 +0530

    Implement binary tree and count leaf nodes

---
+++ Day48 Q1.c
@@ -0,0 +1,64 @@
+// Implement the solution for this problem.
+
+// PROGRAM:
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create node
+struct Node* createNode(int data) {
+    if (data == -1) return NULL;
+
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Build tree from level-order
+struct Node* buildTree(int arr[], int i, int n) {
+    if (i >= n || arr[i] == -1)
+        return NULL;
+
+    struct Node* root = createNode(arr[i]);
+
+    root->left = buildTree(arr, 2*i + 1, n);
+    root->right = buildTree(arr, 2*i + 2, n);
+
+    return root;
+}
+
+// Count leaf nodes
+int countLeaves(struct Node* root) {
+    if (root == NULL)
+        return 0;
+
+    if (root->left == NULL && root->right == NULL)
+        return 1;
+
+    return countLeaves(root->left) + countLeaves(root->right);
+}
+
+int main() {
+    int n;
+
+    scanf("%d", &n);
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct Node* root = buildTree(arr, 0, n);
+
+    printf("%d", countLeaves(root));
+
+    return 0;
+}
