d1bf685b68fa4d6f4a27762d053960bb3d7a0f72
Saksham-Jain-2007 <sakshamjain7017@gmail.com>
2026-03-21 11:55:46 +0530

    Implement polynomial using linked list
    This program implements a polynomial using a linked list with dynamic memory allocation. It allows the user to input coefficients and exponents, and then prints the polynomial.

---
+++ Day30 Q1.c
@@ -0,0 +1,74 @@
+//Polynomial Using Linked List - Implement using linked list with dynamic memory allocation.
+
+// PROGRAM:
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int coeff;
+    int exp;
+    struct Node* next;
+};
+
+// Create node
+struct Node* createNode(int c, int e) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->coeff = c;
+    newNode->exp = e;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Insert at end
+struct Node* insertEnd(struct Node* head, int c, int e) {
+    struct Node* newNode = createNode(c, e);
+
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+// Print polynomial
+void printPolynomial(struct Node* head) {
+    struct Node* temp = head;
+
+    while (temp != NULL) {
+        // Print coefficient and exponent
+        if (temp->exp == 0)
+            printf("%d", temp->coeff);
+        else if (temp->exp == 1)
+            printf("%dx", temp->coeff);
+        else
+            printf("%dx^%d", temp->coeff, temp->exp);
+
+        if (temp->next != NULL)
+            printf(" + ");
+
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, c, e;
+    struct Node* head = NULL;
+
+    // Input
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &c, &e);
+        head = insertEnd(head, c, e);
+    }
+
+    // Output
+    printPolynomial(head);
+
+    return 0;
+}
