248b89b554bd9814a2cabcef5e91f93d135b4a44
Saksham-Jain-2007 <sakshamjain7017@gmail.com>
2026-03-22 22:19:07 +0530

    Create Day50 Q1.c

---
+++ Day50 Q1.c
@@ -0,0 +1,69 @@
+//  BST Search
+
+// PROGRAM:
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* createNode(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Insert into BST
+struct Node* insert(struct Node* root, int val) {
+    if (root == NULL)
+        return createNode(val);
+
+    if (val < root->data)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val);
+
+    return root;
+}
+
+// Search in BST
+int search(struct Node* root, int key) {
+    if (root == NULL)
+        return 0;
+
+    if (root->data == key)
+        return 1;
+
+    if (key < root->data)
+        return search(root->left, key);
+    else
+        return search(root->right, key);
+}
+
+int main() {
+    int n, i, x, key;
+    struct Node* root = NULL;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    scanf("%d", &key);
+
+    if (search(root, key))
+        printf("Found");
+    else
+        printf("Not Found");
+
+    return 0;
+}
